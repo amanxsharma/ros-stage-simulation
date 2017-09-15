2af911be9828533e3b4897f1dae03029450eb0b6
Aman Sharma <aman.sharma728@gmail.com>
2017-09-15 14:48:56 -0400

    Moving for four edges, fix 90 degree turn and travel of half the distance

--- src/my_stage/src/stopper.cpp
+++ src/my_stage/src/stopper.cpp
@@ -4,6 +4,8 @@
 stopper::stopper()
 {
 	keepMoving = true;
+	edgeTraveled = 0;
+	counter = 0;
 	commandPub = node.advertise<geometry_msgs::Twist>("cmd_vel", 10);
 
 	laserSub = node.subscribe("base_scan", 1, &stopper::scanCallback, this);
@@ -15,11 +17,34 @@ void stopper::moveForward(){
 	commandPub.publish(msg);
 }
 
+void stopper::turnLeft(){
+	geometry_msgs::Twist msg;
+	msg.linear.x = 0.0; msg.linear.y = 0.0;
+	msg.angular.z = TURN_SPEED_MPS;
+	t0 = ros::Time::now().toSec();
+	
+	while(current_angle < final_angle){
+		commandPub.publish(msg);
+		t1 = ros::Time::now().toSec();	
+		current_angle = TURN_SPEED_MPS * (t1 - t0);
+		//ROS_INFO_STREAM("t0 = "<<t0<<" t1 = "<<t1);
+		//ROS_INFO_STREAM("Current Angle = "<<current_angle);
+	}
+	edgeTraveled++;
+	keepMoving = true;
+	counter = 0;
+
+}
+
 void stopper::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan) {
 
 	int minIndex = ceil((MIN_SCAN_ANGLE_RAD - scan->angle_min) / scan->angle_increment);
 	int maxIndex = floor((MAX_SCAN_ANGLE_RAD - scan->angle_min) / scan->angle_increment);
-
+	
+	if(counter == 0){
+		distanceOfWall = scan->ranges[minIndex]; //......
+		counter++;
+	}
 	float closestRange = scan->ranges[minIndex];
 	
 	for(int currIndex = minIndex + 1; currIndex <= maxIndex; currIndex++){
@@ -28,11 +53,15 @@ void stopper::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan) {
 		}
 	}
 
-	ROS_INFO_STREAM("Closest range: " << closestRange);
+	ROS_INFO_STREAM("Closest range: " << closestRange <<" distanceOfWall: "<<distanceOfWall);
 
-	if(closestRange < MIN_PROXIMITY_RANGE_M){
+	//if(closestRange < MIN_PROXIMITY_RANGE_M){
+	if(closestRange < distanceOfWall / 2){
 		ROS_INFO("Stop!");
 		keepMoving = false;
+		ROS_INFO("Turn Left!");
+		turnLeft();
+		
 	}
 }
 
@@ -40,7 +69,7 @@ void stopper::startMoving(){
 	ros::Rate rate(10);
 	ROS_INFO("Start Moving");
 
-	while(ros::ok() && keepMoving){
+	while(ros::ok() && keepMoving && edgeTraveled < 4){
 		moveForward();
 		ros::spinOnce();
 
--- src/my_stage/src/stopper.h
+++ src/my_stage/src/stopper.h
@@ -4,9 +4,13 @@
 class stopper {
 public:
 	const static double FORWARD_SPEED_MPS = 0.5;
+	const static double TURN_SPEED_MPS = 1.57; //90 degrees equals 1.57 rad
 	const static double MIN_SCAN_ANGLE_RAD = -30.0/180*M_PI;
 	const static double MAX_SCAN_ANGLE_RAD = +30.0/180*M_PI;
 	const static float MIN_PROXIMITY_RANGE_M = 0.5;    
+	float current_angle = 0;
+	const static float final_angle = 1.8; //..............FIX THIS FOR TURN
+	int t1, t0;
 
 	stopper();
 	void startMoving();
@@ -16,7 +20,11 @@ private:
 	ros::Publisher commandPub;
 	ros::Subscriber laserSub;
 	bool keepMoving;
+	int edgeTraveled;
+	int counter;
+	float distanceOfWall;
  
 	void moveForward();
+	void turnLeft();
 	void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
 };
