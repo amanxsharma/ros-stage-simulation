d2b783526374938a17546f66476210b5f82cbed2
Aman Sharma <aman.sharma728@gmail.com>
2017-10-07 17:10:33 -0400

    working perfect

--- src/line_follower/src/line_follower.h
+++ src/line_follower/src/line_follower.h
@@ -14,19 +14,20 @@ public:
 
   void publishMessage();
 
-
+  void turn();
   void messageCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
 
 //variables
+  int counter;
   double wallDistance; // Desired distance from the wall.
-  double e;            // Difference between desired distance from the wall and actual distance.
+  double e,t0,t1;            // Difference between desired distance from the wall and actual distance.
   double diffE;     // Derivative element for PD controller;
   double maxSpeed;     // Maximum speed of robot.
   double P = 10;            // k_P Constant for PD controller.
   double D = 5;            // k_D Constant for PD controller.
   double angleCoef;    // Coefficient for P controller.
   int direction;      // 1 for wall on the right side of the robot (-1 for the left one).
-  double angleMin;     // Angle, at which was measured the shortest distance.
+  double angleMin, am;     // Angle, at which was measured the shortest distance.
   double distFront;    // Distance, measured by ranger in front of robot.
   ros::Publisher pubMessage;  // Object for publishing messages.
 
--- src/line_follower/src/working_line_follower.cpp
+++ src/line_follower/src/working_line_follower.cpp
@@ -3,18 +3,19 @@
 #define PI 3.141592
 //without fabs wala elseif it works...
 
+//LineFollowing::LineFollowing(ros::Publisher pub, double wallDist, double maxSp, int dir, double pr, double di, double an)
 LineFollowing::LineFollowing()
 {
-//ROS_INFO("constructor called");
-  wallDistance = 0.5;//1.0;//wallDist;
+ROS_INFO("constructor called");
+  wallDistance = 1.0;//wallDist;
   maxSpeed = 0.1;//maxSp;
-
+  //direction = -1;//dir;
   P = 10;//pr;
   D = 5;//di;
-
+  //angleCoef = an;
   e = 0;
   angleMin = 0;  //angle, at which was measured the shortest distance
-
+//  pubMessage = pub;
  sub = nodeHandle.subscribe("base_scan", 1, &LineFollowing::messageCallback, this);
  pubMessage = nodeHandle.advertise<geometry_msgs::Twist>("cmd_vel", 1000);//PUBLISHER_BUFFER_SIZE);
 
@@ -24,33 +25,62 @@ int main(int argc, char **argv)
 {
   //Initialization of node
   ros::init(argc, argv, "wallFollowing");
-  LineFollowing *lineFollowing = new LineFollowing(); //1 for right wall  
+
+  //ros::NodeHandle n;
+  //Creating publisher
+  
+
+  //Creating object, which stores data from sensors and has methods for
+  //publishing and subscribing
+//  LineFollowing *lineFollowing = new LineFollowing(pubMessage, WALL_DISTANCE, MAX_SPEED, DIRECTION, P, D, 1);
+  LineFollowing *lineFollowing = new LineFollowing(); //1 for right wall
+//LineFollowing lineFollowing; //1 for right wall
+
+  //Creating subscriber and publisher
+//  ros::Subscriber sub = n.subscribe("base_scan", 1, &LineFollowing::messageCallback, lineFollowing);
+  
   ros::spin();
 
   return 0;
+
+/*
+double P = 10;            // k_P Constant for PD controller.
+double D = 5;            // k_D Constant for PD controller.
+const static int SUBSCRIBER_BUFFER_SIZE = 1;  // Size of buffer for subscriber.
+const static int PUBLISHER_BUFFER_SIZE = 1000;  // Size of buffer for publisher.
+const static double WALL_DISTANCE = 0.13;
+const static double MAX_SPEED = 0.1;
+const static int ANGLE_COEF = 1;    // Proportional constant for angle controller
+const static int DIRECTION = 1; // 1 for wall on the left side of the robot (-1 for the right side).
+*/
 }
 
+/*LineFollowing::~LineFollowing()
+{
+}
+*/
 //Publisher
 void LineFollowing::publishMessage()
 {
   //preparing message
   geometry_msgs::Twist msg;
 
+//double temp = direction*(P*e + D*diffE) + (angleMin - PI*direction/2);
 double temp =  (angleMin + PI/2) - (P*e + D*diffE);
 //ROS_INFO_STREAM("direc: "<<direction<<" e: "<<e<<" diffE: "<<diffE<<" angleMin: "<<angleMin<<" z: "<<temp);
 
   msg.angular.z = temp;    //PD controller
 
   if (distFront < wallDistance){
-    msg.linear.x = 0;				//start turning
+    msg.linear.x = 0;
   }
   else if (distFront < wallDistance * 2){
-    msg.linear.x = 0.5*maxSpeed;		//slow down
+    msg.linear.x = 0.5*maxSpeed;
   }
-  else if (fabs(angleMin)>1.75){
+  /*else if (fabs(angleMin)>1.75){
     msg.linear.x = 0.4*maxSpeed;
 	ROS_INFO("fabs");
-  } 
+  } */
   else {
     msg.linear.x = maxSpeed;
   }
@@ -62,12 +92,16 @@ double temp =  (angleMin + PI/2) - (P*e + D*diffE);
 //Subscriber
 void LineFollowing::messageCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-  //ROS_INFO("inside callback");
+  ROS_INFO("inside callback");
   int size = msg->ranges.size();
 
   //Variables whith index of highest and lowest value in array.
+//  int minIndex = size*(direction+1)/4;	
+//  int maxIndex = size*(direction+3)/4;
+
   int minIndex = 0;//size*(direction+1)/4;	
   int maxIndex = size/2;//size*(direction+3)/4;
+
   
   //This cycle goes through array and finds minimum
   for(int i = minIndex; i < maxIndex; i++)
@@ -84,9 +118,10 @@ void LineFollowing::messageCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
   distMin = msg->ranges[minIndex];   //I think this is right Wall
 
   distFront = msg->ranges[size/2];   //300 CHANGED...originally it was size/2, but my robot is inclined
-  //ROS_INFO_STREAM("distFront: "<<distFront);
+  ROS_INFO_STREAM("distFront: "<<distFront);
   diffE = (distMin - wallDistance) - e;
   e = distMin - wallDistance;
+  //ROS_INFO("now move!");
   //Invoking method for publishing message
   publishMessage();
 }
